8b85caa6aff8db37c0faeabc7bc5c37f86cbbf98
PRAKASH ANAND <34940322+GHATAK123@users.noreply.github.com>
2020-06-12 12:26:24 +0530

    Create Sort Colors.java

---
+++ Sort Colors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+       //  ArrayList l=new ArrayList();
+       //  for(int i=0;i<nums.length;i++)
+       //  {
+       //      if(nums[i]==0)
+       //          l.add(nums[i]);
+       //  }
+       //  for(int i=0;i<nums.length;i++)
+       //  {
+       //      if(nums[i]==1)
+       //          l.add(nums[i]);
+       //  }
+       //  for(int i=0;i<nums.length;i++)
+       //  {
+       //      if(nums[i]==2)
+       //          l.add(nums[i]);
+       //  }
+       // System.out.println(l);
+        //Iterator itr=l.iterator();
+        // while(itr.hasNext())
+        // {
+        //     Integer I=(Integer)itr.next();
+        //     System.out.println(I + " ");
+        // }
+    }
+}
